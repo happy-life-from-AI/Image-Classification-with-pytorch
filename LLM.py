96af6b040c658a2bfa193684fe84226338e8bc21
JH CHOO <knocklovelucy@gmail.com>
2025-10-29 15:15:53 +0900

    2025.10.29일 Git 및 ollama 공부 자료

--- LLM.py
+++ LLM.py
@@ -3,5 +3,41 @@
 Created on Wed Oct 29 14:42:21 2025
 
 @author: jeonghwan1117
+@name  : LLM Based classificaiton with llava
 """
+
+from ollama import generate
+
+from PIL import Image
+from io  import BytesIO
+
+
+def load_image(image_path):
+    '''
+    Parameters
+    ----------
+    image_path : Image Path
+
+    Returns
+    -------
+    image_bytes
+
+    '''
+    with Image.open(image_path) as img:
+        with BytesIO() as bf:
+            img.save(bf, format = 'PNG')
+            img_b = bf.getvalue()
+            
+
+    return img_b
+
+
+
+
+if __name__ == "__main__":
+
+    image = "./Images/Test.JPG"
+
+
+
 
